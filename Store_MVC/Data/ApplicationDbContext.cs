45cfa215415c0b2c2e9293fc9246048902251cbf
m-anass <manas8269@gmail.com>
2024-09-22 09:16:08 +0300

    Create DataBase

---
+++ Store_MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Store_MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- Store_MVC/Program.cs
+++ Store_MVC/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Store_MVC.Data;
+
 namespace Store_MVC
 {
     public class Program
@@ -8,6 +11,9 @@ namespace Store_MVC
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
             var app = builder.Build();
 
