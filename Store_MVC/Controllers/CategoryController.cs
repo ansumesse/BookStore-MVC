e43275d2bd5af56ace46871cf4641d5e40a8712b
m-anass <manas8269@gmail.com>
2024-09-22 10:44:15 +0300

    Add Category Controller And View

---
+++ Store_MVC/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store_MVC.Data;
+using Store_MVC.Models;
+
+namespace Store_MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = db.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
