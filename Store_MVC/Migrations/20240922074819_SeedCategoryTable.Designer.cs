daf3f06ea91d32ea46d8260398b13e9be29623ec
m-anass <manas8269@gmail.com>
2024-09-22 10:48:52 +0300

    Seed Category Table

--- Store_MVC/Migrations/20240922074819_SeedCategoryTable.Designer.cs
+++ Store_MVC/Migrations/20240922074819_SeedCategoryTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Store_MVC.Data;
 
@@ -10,9 +11,11 @@ using Store_MVC.Data;
 namespace Store_MVC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240922074819_SeedCategoryTable")]
+    partial class SeedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
