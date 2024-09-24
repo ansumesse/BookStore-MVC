9ed4523d866f9cbe20dd80a5ebe898220f5b44f9
m-anass <manas8269@gmail.com>
2024-09-24 13:48:51 +0300

    Add CategoryRepository

---
+++ Store.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using Store.DataAccess.Data;
+using Store.DataAccess.Repository.IRepository;
+using Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.DataAccess.Repository
+{
+    internal class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoryRepository(ApplicationDbContext db): base(db)
+        {
+            this.db = db;
+        }
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            db.Categories.Update(category);
+        }
+    }
+}
