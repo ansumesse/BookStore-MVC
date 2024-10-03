3eaaa92da1e73e7989b3921f4d43fd94feb58842
m-anass <manas8269@gmail.com>
2024-10-03 17:58:05 +0300

    Add Order Models

--- Store.DataAccess/Data/ApplicationDbContext.cs
+++ Store.DataAccess/Data/ApplicationDbContext.cs
@@ -147,6 +147,8 @@ namespace Store.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
     }
 }
---
+++ Store.Models/OrderDetail.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("OrderHeader")]
+        public int OrderHeaderId { get; set; }
+        public OrderHeader? OrderHeader { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int Count { get; set; }
+        public double Price { get; set; } // Product have Price too but it may change with time so Order Price Cannot Change after User order
+    }
+}
