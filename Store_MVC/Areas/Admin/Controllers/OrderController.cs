19ebae837139bf77723dac6fff0870b049e9037a
m-anass <manas8269@gmail.com>
2024-10-05 19:36:07 +0300

    Create Order Mangement Controller and view

---
+++ Store_MVC/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.DataAccess.Repository;
+using Store.DataAccess.Repository.IRepository;
+using Store.Models;
+
+namespace Store_MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<OrderHeader> orders = unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return View(orders);
+        }
+    }
+}
