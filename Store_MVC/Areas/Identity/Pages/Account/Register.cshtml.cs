6ed85bac553502f5e222cabee8c966d9a1e60216
m-anass <manas8269@gmail.com>
2024-10-11 19:59:40 +0300

    Creating Admin and Employee Accounts

--- Store_MVC/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Store_MVC/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -204,8 +204,15 @@ namespace Store_MVC.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
+						if (User.IsInRole(SD.Role_Admin))
+						{
+							TempData["success"] = "New User Created Successfully";
+						}
+						else
+						{
+							await _signInManager.SignInAsync(user, isPersistent: false);
+						}
+						return LocalRedirect(returnUrl);
                     }
                 }
                 foreach (var error in result.Errors)
